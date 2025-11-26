328fdb3e1204111cd2ce52ad8adf519fda02445f
Abdi Mohamud <muhamud12@hotmail.com>
2025-11-26 20:58:16 +0200

    update

--- arrays/exercise.js
+++ arrays/exercise.js
@@ -1,22 +1,22 @@
-// sum
+// sum of numbers in an array
 
-const addNum = [10, 20, 30]
-let num = 0
+const addNum = [10, 20, 30];
+let num = 0;
 for (let i = 0; i < addNum.length; i++) {
-  num = num + addNum[i]
+  num = num + addNum[i];
 }
-console.log("sum is " + num)
+console.log("sum is " + num); // sum is 60
 
 // even numbers to a new array push
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8]
-const evenNumbers = []
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+const evenNumbers = [];
 
 for (let i = 0; i < numbers.length; i++) {
-  const current = numbers[i]
+  const current = numbers[i];
   // jos current on parillinen, lisää evenNumbersiin
   if (current % 2 === 0) {
-    evenNumbers.push(current)
+    evenNumbers.push(current);
   }
 }
 
-console.log(evenNumbers)
+console.log(evenNumbers);
